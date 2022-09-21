1b5ed7e089d4524ca682a4a27345b1f97dc251f6
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-21 13:35:12 +0600

    sdsadasd

--- install.sh
+++ install.sh
@@ -8,3 +8,9 @@ chattr -i /usr/share/pixmaps/ubuntu-logo-dark.png
 chattr -i /usr/share/pixmaps/ubuntu-logo-icon.png
 cp  ./etc /
 cp ./usr /
+chattr +i /usr/lib/os-release
+chattr +i /etc/lsb-release
+chattr +i /etc/issue
+chattr +i /etc/issue.net
+chattr +i /usr/share/pixmaps/ubuntu-logo-dark.png
+chattr +i /usr/share/pixmaps/ubuntu-logo-icon.png
