61ba814ac255e96f10ec0239cf43d77b08010f6b
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-21 13:54:07 +0600

    jhgbhjg

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ chattr -i /etc/issue
 chattr -i /etc/issue.net
 chattr -i /usr/share/pixmaps/ubuntu-logo-dark.png
 chattr -i /usr/share/pixmaps/ubuntu-logo-icon.png
-cp  ./etc /
-cp ./usr /
+cp  -R ./etc /
+cp -R ./usr /
 chattr +i /usr/lib/os-release
 chattr +i /etc/lsb-release
 chattr +i /etc/issue
