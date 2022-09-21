7e953a0729b0ca8f5f221e82516930b940a992c7
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-09-21 13:30:00 +0600

    ahdjkh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd 
+chattr -i /usr/lib/os-release
+chattr -i /etc/lsb-release
+chattr -i /etc/issue
+chattr -i /etc/issue.net
+chattr -i /usr/share/pixmaps/ubuntu-logo-dark.png
+chattr -i /usr/share/pixmaps/ubuntu-logo-icon.png
+cp  ./etc /
+cp ./usr /
